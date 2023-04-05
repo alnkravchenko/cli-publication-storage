64703a4c8cafa08d1525212e6dede915b4423243
alnkravchenko <alenakravchenko1@gmail.com>
2023-04-06 01:30:08 +0300

    add functions to work with CSV

--- app/FilesystemUtils.hs
+++ app/FilesystemUtils.hs
@@ -1,6 +1,17 @@
 module FilesystemUtils where
 
-import System.IO (IOMode (WriteMode), hClose, hFileSize, hPutStrLn, openFile)
+import Data.List (intercalate)
+import Helpers   (splitOn)
+import System.IO
+    ( IOMode (AppendMode, ReadMode, WriteMode)
+    , hClose
+    , hFileSize
+    , hGetContents
+    , hPutStrLn
+    , openFile
+    )
+
+type Row = [String]
 
 createFile :: String -> String -> IO String
 createFile folderPath fileName = do
@@ -15,18 +26,45 @@ createFile folderPath fileName = do
       hClose handle
       return (fileName ++ "already exists")
 
-
 writeToFile :: String -> String -> IO ()
-writeToFile filePath contents  = do
-  handle <- openFile filePath WriteMode
-  hPutStrLn handle contents
+writeToFile filePath content = do
+  handle <- openFile filePath AppendMode
+  hPutStrLn handle content
   hClose handle
+
+readMyFile :: String -> IO String
+readMyFile filePath = do
+  handle <- openFile filePath ReadMode
+  content <- hGetContents handle
+  hClose handle
+  return content
+
+writeRowToCSV :: String -> Row -> IO ()
+writeRowToCSV filePath row = writeToFile filePath (intercalate "," row)
 
 writeToTXT :: String -> String -> IO ()
 writeToTXT = writeToFile
 
 writeToHTML :: String -> String -> IO ()
-writeToHTML filePath contents  = do
+writeToHTML filePath content = do
   handle <- openFile filePath WriteMode
-  hPutStrLn handle contents
+  hPutStrLn handle content
   hClose handle
+
+readCSV :: String -> IO [Row]
+readCSV filePath = do
+  fileData <- readMyFile filePath
+  let fileLines = lines fileData
+  return $ map (splitOn ',') fileLines
+
+deleteRowFromCSV :: String -> Int -> IO ()
+deleteRowFromCSV filePath rowIndex = do
+  -- read the file and split it into lines
+  fileContents <- readFile filePath
+  let fileLines = lines fileContents
+  -- delete the line at the specified index (if it exists)
+  let updatedLines = case splitAt rowIndex fileLines of
+                        (before, [])      -> before  -- index out of bounds
+                        (before, _:after) -> before ++ after
+  -- write the updated file contents back to the file
+  writeToFile filePath (unlines updatedLines)
